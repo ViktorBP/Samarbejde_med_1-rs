ac1fcdbf6d5a1ae56e7026f9a8490a5750ecc719
rschapiro <rasmus.schapiro@gmail.com>
2021-10-25 13:58:53 +0200

    added ProductRepo Class + streamwrite and remove products methods

---
+++ TværfagligtProjekt/Repositories/ProductRepo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using ModelLibrary;
+using System.IO;
+
+namespace Repositories
+{
+    public class ProductRepo
+    {
+        static string ProductPath = "ProductList.txt";
+
+        private List<Product> products = new List<Product>();
+
+        public void AddProduct(Product product)
+        {
+            products.Add(product);
+        }
+
+        public void SaveProducts(List<Product> products)
+        {
+            StreamWriter sw = new StreamWriter(ProductPath, append: true);
+
+            foreach (var p in products)
+            {
+                sw.WriteLine(p.ToString());
+            }
+            sw.Close();
+        }
+
+        public void RemoveAllProducts(List<Product> products)
+        {
+            products.Clear();
+        }
+    }
+}
