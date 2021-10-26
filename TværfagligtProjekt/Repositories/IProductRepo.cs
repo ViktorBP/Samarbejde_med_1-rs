b258923178b10e772bb78d63b9c9ccfd78305052
rschapiro <rasmus.schapiro@gmail.com>
2021-10-26 09:48:07 +0200

    added IProductRepo interface

---
+++ TværfagligtProjekt/Repositories/IProductRepo.cs
@@ -0,0 +1,18 @@
+﻿using ModelLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repositories
+{
+    public interface IProductRepo
+    {
+        public List<Product> Products { get; set; }
+
+        public void AddProduct(Product product);
+        public Product GetProduct(int Id);
+        public List<Product> GetAllProducts();
+        public Product UpdateProduct(int Id);
+        public void DeleteProduct(int Id);
+    }
+}
--- TværfagligtProjekt/Repositories/ProductRepo.cs
+++ TværfagligtProjekt/Repositories/ProductRepo.cs
@@ -5,15 +5,19 @@ using System.IO;
 
 namespace Repositories
 {
-    public class ProductRepo
+    public class ProductRepo : IProductRepo
     {
         static string ProductPath = "ProductList.txt";
 
         private List<Product> products = new List<Product>();
 
+        public List<Product> Products { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
         public void AddProduct(Product product)
         {
             products.Add(product);
+
+            // add streamwrite
         }
 
         public void SaveProducts(List<Product> products)
@@ -30,6 +34,28 @@ namespace Repositories
         public void RemoveAllProducts(List<Product> products)
         {
             products.Clear();
+        }
+
+
+
+        public Product GetProduct(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product UpdateProduct(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteProduct(int Id)
+        {
+            throw new NotImplementedException();
         }
     }
 }
